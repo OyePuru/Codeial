764fc5af1328003ac6639ea69656dd1a353816ac
oyepuru <mandalpurushottam66@gmail.com>
2021-12-05 14:34:25 +0100

    Displaying posts and name of the user posted

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,8 @@
-module.exports.home = function(req, res){
-    return res.render('home', {
-        title: "Home"
-    });
+const Post = require('../models/post.js')
+module.exports.home = function(req,res){
+  Post.find({}).populate('user').exec(function(err,posts){
+    return res.render('home',{
+      title : "Codeial | Home", posts : posts
+    })
+  })
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,6 +1,10 @@
 const Post = require('../models/post')
 
 module.exports.create = function(req, res){
+    if(!req.user){
+      return res.redirect('back')
+    }
+
     Post.create({
         content: req.body.content,
         user: req.user._id
